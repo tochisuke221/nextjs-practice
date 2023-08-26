60952f8a107cb8f4b561af6118ee359402a09d07
tochisuke221 <tochisuke221@gmail.com>
2023-08-27 01:25:24 +0900

    useRefの復習

--- react-practice/src/App.tsx
+++ react-practice/src/App.tsx
@@ -1,36 +1,28 @@
-import React, { useState, useCallback, FunctionComponent } from 'react';
+import React, { useState, useRef  } from 'react';
 import './App.css';
 
-
-const DisplayResult: FunctionComponent <{
-  findValue: (query: string) => string;
-  query: string;
-}> = ({findValue, query}) => {
-  return <div>{findValue(query)}</div>
-}
 
 function App() {
   const [inputValue, setInputValue] = useState<string>('default value')
 
-  const findValue = useCallback(
-    (query: string) => {
-      return inputValue.indexOf(query) === -1
-      ? `${query} not found`
-      : `${query} found`
-    },
-    [inputValue]
-  )
+  const inputRef = useRef<HTMLInputElement>(null)
 
   return (
     <div className="App">
       <input
+        ref={inputRef}
         value={inputValue}
         onChange={e => {
           setInputValue(e.target.value);
         }}
       />
-      <DisplayResult findValue={findValue} query="hoge"/>
-      <DisplayResult findValue={findValue} query="fuga"/>
+      <button
+        onClick={()=>{
+          inputRef.current?.focus();
+        }}
+      >
+        focus
+      </button>
 
     </div>
   );
