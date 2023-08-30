32ad93e4a54c399ec632d9d0b70983ff6ede9c64
KousukeTochikawa <tochikawa.kousuke@sumasate.jp>
2023-08-30 23:31:05 +0900

    修正等

--- ts-practice/index.ts
+++ ts-practice/index.ts
@@ -1,185 +1,203 @@
 
 
-// let anyVariable: any = 1
-// anyVariable = 'string'
-// anyVariable = true
+// // let anyVariable: any = 1
+// // anyVariable = 'string'
+// // anyVariable = true
 
-// // エラーにならない
-// let numberVariable: number = anyVariable;
-
-
-// let unkwon: unknown = -1;
-
-// // エラーになる
-// numberVariable = unkwon
-
-// // ■ まとめ
-// // anyは型チェックが入らない
-
-const object1: { 
-  num: number,
-  str?: string
-} = {
-  num: 1
-}
-
-console.log(object1.num)
-console.log(object1.str) // => undefined
+// // // エラーにならない
+// // let numberVariable: number = anyVariable;
 
 
-// オプショナルチェーン
-// Rubyでいうぼっち演算子
-let obj: any = undefined;
-obj?.test //=> undefinedとなる
+// // let unkwon: unknown = -1;
 
-// オブジェクトを代入した変数は常に参照として振る舞う
+// // // エラーになる
+// // numberVariable = unkwon
 
-const obj1 = {
-  key1: 1,
-  key2: 'string'
-}
+// // // ■ まとめ
+// // // anyは型チェックが入らない
 
-obj1.key1 = 3
+// const object1: { 
+//   num: number,
+//   str?: string
+// } = {
+//   num: 1
+// }
 
-const obj2 = obj1
+// console.log(object1.num)
+// console.log(object1.str) // => undefined
 
-obj2.key2 = 'string2'
 
-console.log(obj1)
-//=> { key1: 3, key2: 'string2' }
+// // オプショナルチェーン
+// // Rubyでいうぼっち演算子
+// let obj: any = undefined;
+// obj?.test //=> undefinedとなる
 
-// 配列
+// // オブジェクトを代入した変数は常に参照として振る舞う
 
-const aryNums: number[] = [0, 1, 2]
-const aryStrings: string[] = ['str1', 'str2']
+// const obj1 = {
+//   key1: 1,
+//   key2: 'string'
+// }
 
-// 普通type使わね？
-// const objeArgFunc = (arg: {
-//   strVal: 'string'
-// }): boolean => {
-//   if(arg.strVal === "str1"){
+// obj1.key1 = 3
+
+// const obj2 = obj1
+
+// obj2.key2 = 'string2'
+
+// console.log(obj1)
+// //=> { key1: 3, key2: 'string2' }
+
+// // 配列
+
+// const aryNums: number[] = [0, 1, 2]
+// const aryStrings: string[] = ['str1', 'str2']
+
+// // 普通type使わね？
+// // const objeArgFunc = (arg: {
+// //   strVal: 'string'
+// // }): boolean => {
+// //   if(arg.strVal === "str1"){
+// //     return true;
+// //   }
+
+// //   return false
+// // }
+
+// type ArgumentType = {
+//   strVal: string;
+// };
+
+// const objeArgFunc = (arg: ArgumentType): boolean => {
+//   if (arg.strVal === "str1") {
 //     return true;
 //   }
 
-//   return false
+//   return false;
+// };
+
+// // ジェネリクス
+// const genericFunc = <T>(arg: T): T => {
+//   return arg
 // }
 
-type ArgumentType = {
-  strVal: string;
-};
+// // T型を引数や戻り値の型解釈に使える。
+// genericFunc<string>('string') 
+// // 推測してくれる
+// genericFunc('string') 
+// genericFunc(123) 
 
-const objeArgFunc = (arg: ArgumentType): boolean => {
-  if (arg.strVal === "str1") {
-    return true;
-  }
-
-  return false;
-};
-
-// ジェネリクス
-const genericFunc = <T>(arg: T): T => {
-  return arg
-}
-
-// T型を引数や戻り値の型解釈に使える。
-genericFunc<string>('string') 
-// 推測してくれる
-genericFunc('string') 
-genericFunc(123) 
-
-// 型もジェネリックにできる
-type Generic<T> = {
-  value: T
-}
-
-const genericTypeFunc: Generic<string> = {
-  value: 'string'
-}
-
-type NewType = {
-  number: number,
-  string: string
-}
-
-type PartialNewType = Partial<NewType>
-//
-// {
-//   number?: number || undefined;
-//   string?: string || undefined;
+// // 型もジェネリックにできる
+// type Generic<T> = {
+//   value: T
 // }
-//
-type RequireNewType = Required<NewType>
-// {
-  //   number: number;
-  //   string: string;
-  // }
-type PickNewType = Pick<NewType, 'number'>
-// {
-//   number: number
+
+// const genericTypeFunc: Generic<string> = {
+//   value: 'string'
 // }
-type OmitNewType = Omit<NewType, 'number'>
-// {
+
+// type NewType = {
+//   number: number,
 //   string: string
 // }
 
-type UnionType = 'a' | 'b' | 'c'
+// type PartialNewType = Partial<NewType>
+// //
+// // {
+// //   number?: number || undefined;
+// //   string?: string || undefined;
+// // }
+// //
+// type RequireNewType = Required<NewType>
+// // {
+//   //   number: number;
+//   //   string: string;
+//   // }
+// type PickNewType = Pick<NewType, 'number'>
+// // {
+// //   number: number
+// // }
+// type OmitNewType = Omit<NewType, 'number'>
+// // {
+// //   string: string
+// // }
 
-type ExcludeUnionType = Exclude<UnionType, 'a' | 'b'>
-// 'c'
-type ExtractUnionType = Extract<UnionType, 'a' | 'b'>
-// 'a' | 'b'
+// type UnionType = 'a' | 'b' | 'c'
 
-type ObjType = {
-  key1: 'value1',
-  3: 'value3'
+// type ExcludeUnionType = Exclude<UnionType, 'a' | 'b'>
+// // 'c'
+// type ExtractUnionType = Extract<UnionType, 'a' | 'b'>
+// // 'a' | 'b'
+
+// type ObjType = {
+//   key1: 'value1',
+//   3: 'value3'
+// }
+
+// type ObjectkeyType = keyof ObjType
+// // 'key1' | 3
+
+// const objFunction = (
+//   object: ObjType,
+//   key: keyof ObjType
+// ) => {
+//   return object[key]
+// }
+
+// objFunction(
+//   {
+//     key1: 'value1',
+//     3: 'value3'
+//   },
+//   'key1'
+// )
+
+// const object = {
+//   key3: 1,
+//   key4:2
+// }
+
+// type ObjectKey = keyof typeof object 
+
+// // プロパティを任意の文字列や数値にできる
+// type StringkeyType = {
+//   [k: string]: string;
+// }
+
+// const attributeKey: StringkeyType = {
+//   anyString:'string'
+// }
+
+// type UnionKeyObj = {
+//   [k in UnionType]: number;
+// }
+
+// type ObjTypeKeyType = {
+//   [k in keyof ObjType]: number;
+// }
+
+// const object3: ObjTypeKeyType = {
+//   key1: 1,
+//   3: 3
+// }
+
+
+
+
+interface AddressElement {
+  address? : string;
+  prefecture? :string;
+  region?: string;
+  city?: string;
+  jgdc_postal_code? : string;
+  latitude? : number;
+  longitude?: number;
+  postal_code?: string;
 }
 
-type ObjectkeyType = keyof ObjType
-// 'key1' | 3
 
-const objFunction = (
-  object: ObjType,
-  key: keyof ObjType
-) => {
-  return object[key]
-}
+let address = null as AddressElement
+console.log(address)
 
-objFunction(
-  {
-    key1: 'value1',
-    3: 'value3'
-  },
-  'key1'
-)
-
-const object = {
-  key3: 1,
-  key4:2
-}
-
-type ObjectKey = keyof typeof object 
-
-// プロパティを任意の文字列や数値にできる
-type StringkeyType = {
-  [k: string]: string;
-}
-
-const attributeKey: StringkeyType = {
-  anyString:'string'
-}
-
-type UnionKeyObj = {
-  [k in UnionType]: number;
-}
-
-type ObjTypeKeyType = {
-  [k in keyof ObjType]: number;
-}
-
-const object3: ObjTypeKeyType = {
-  key1: 1,
-  3: 3
-}
-
-
-
+// address = null 
+// console.log(address)
