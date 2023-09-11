602e7abe29142a24a9182309a96cf7ffe100f5bc
tochisuke221 <tochisuke221@gmail.com>
2023-09-11 20:12:59 +0900

    作業中

--- nextjs-sample/src/pages/index.tsx
+++ nextjs-sample/src/pages/index.tsx
@@ -59,86 +59,98 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => {
     });
 
     const posts: Post[] = [];
-
-    for(const page of database.results) {
-      if(!('properties' in page)) {
-         posts.push({
-          id: page.id,
-          title: null,
-          slug: null,
-          createdTs: null,
-          lastEditedTs: null,
-          contents: []
-         });
-         continue;
-        }
-      }
-  
-      let title: string | null = null
-
-      if(page.properties['Name'].type === 'title'){
-        title = page.properties['Name'].title[0]?.plain_text ?? null ;
-      }
-      
-      let slug: string | null  = null;
-      
-      if(page.properties['Slug'].type === 'rich_text') {
-        slug = page.properties['Slug'].rich_text[0]?.plain_text ?? null ;
-      }
-  
-      const blocks = await notion.blocks.children.list({
-        block_id: database.results[0]?.id
+    const blockResponses = await Promise.all(
+      database.results.map((page) => {
+        return notion.blocks.children.list({
+          block_id: page.id
+        })
       })
-  
-      const contents: Content[] = [];
-  
-      blocks.results.forEach(block => {
-        if(!('type' in block)){
-          return;
-        }
-  
-        switch(block.type){
-          case 'paragraph':
-            contents.push({
-              type: 'paragraph',
-              text: block.paragraph.rich_text[0]?.plain_text ?? null
-            });
-            break;
-          case 'heading_2':
-             contents.push({
-               type: 'heading_2',
-               text: block.heading_2.rich_text[0]?.plain_text ?? null
-             });
-             break;
-          case 'heading_3':
-             contents.push({
-               type: 'heading_3',
-               text: block.heading_3.rich_text[0]?.plain_text ?? null
-             });
-             break;
-          case 'quote':
-             contents.push({
-               type: 'heading_3',
-               text: block.quote.rich_text[0]?.plain_text ?? null
-             });
-             break;
-          case 'code':
-             contents.push({
-               type: 'code',
-               text: block.code.rich_text[0]?.plain_text ?? null,
-               language: block.code.language
-             });
-        }
-      });
+    )
+    
+    database.results.forEach((index, page) => {
+      console.dir(page)
+    }
+    )
+    database.results.forEach((index, page) => {
+      console.dir(page)
+      // if(!('properties' in page)) {
+      //   posts.push({
+      //    id: page.id,
+      //    title: null,
+      //    slug: null,
+      //    createdTs: null,
+      //    lastEditedTs: null,
+      //    contents: []
+      //   })
+      // }
 
-      posts.push({
-        id: page.id,
-        title,
-        slug,
-        createdTs: page.created_time,
-        lastEditedTs: page.last_edited_time,
-        contents
-      })
+      // let title: string | null = null
+
+      // if(page.properties['Name'].type === 'title'){
+      //   title = page.properties['Name'].title[0]?.plain_text ?? null ;
+      // }
+  
+      // let slug: string | null  = null;
+   
+      // if(page.properties['Slug'].type === 'rich_text') {
+      //   slug = page.properties['Slug'].rich_text[0]?.plain_text ?? null ;
+      // }
+  
+      // const blocks = blockResponses[index];
+  
+      // const contents: Content[] = [];
+  
+      // blocks.results.forEach(block => {
+      //  if(!('type' in block)){
+      //    return;
+      //  }
+  
+      //  switch(block.type){
+      //    case 'paragraph':
+      //      contents.push({
+      //        type: 'paragraph',
+      //        text: block.paragraph.rich_text[0]?.plain_text ?? null
+      //      });
+      //      break;
+      //    case 'heading_2':
+      //       contents.push({
+      //         type: 'heading_2',
+      //         text: block.heading_2.rich_text[0]?.plain_text ?? null
+      //       });
+      //       break;
+      //    case 'heading_3':
+      //       contents.push({
+      //         type: 'heading_3',
+      //         text: block.heading_3.rich_text[0]?.plain_text ?? null
+      //       });
+      //       break;
+      //    case 'quote':
+      //       contents.push({
+      //         type: 'heading_3',
+      //         text: block.quote.rich_text[0]?.plain_text ?? null
+      //       });
+      //       break;
+      //    case 'code':
+      //       contents.push({
+      //         type: 'code',
+      //         text: block.code.rich_text[0]?.plain_text ?? null,
+      //         language: block.code.language
+      //       });
+      //  }
+      // });
+  
+      // posts.push({
+      //   id: page.id,
+      //   title,
+      //   slug,
+      //   createdTs: page.created_time,
+      //   lastEditedTs: page.last_edited_time,
+      //   contents
+      // })
+    })
+
+    return {
+      props: { posts }
     }
 };
 
