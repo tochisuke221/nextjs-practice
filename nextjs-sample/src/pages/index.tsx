f89fd23058f875aa1348d80f2781298be38c896b
tochisuke221 <tochisuke221@gmail.com>
2023-09-12 19:43:15 +0900

    個別詳細ページ

--- nextjs-sample/src/pages/index.tsx
+++ nextjs-sample/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import styles from '../styles/Home.module.css'
 import prism from 'prismjs';
 import dayjs from 'dayjs';
+import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
 
 export type Content =
   {
@@ -29,13 +30,140 @@ export type Post = {
   contents: Content[];
 };
 
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN
+});
+
+export const getPosts = async (slug?: string) => {
+  let database: QueryDatabaseResponse | undefined = undefined;
+
+  if(slug) {
+    database = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID || '',
+      filter: {
+        and: [
+          {
+            property: 'Slug',
+            rich_text: {
+              equals: slug
+            }
+          }
+        ]
+      }
+    });
+  } else {
+    database = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID || '',
+      filter: {
+        and: [
+          {
+            property: 'Published',
+            checkbox: {
+              equals: true
+            }
+          }
+        ]
+      },
+      sorts: [
+        {
+          timestamp: 'created_time',
+          direction: 'descending'
+        }
+      ]
+    });
+  }
+  if(!database) return [];
+  const posts: Post[] = [];
+
+  database.results.forEach((page, index) => {
+    if(!('properties' in page)) {
+      posts.push({
+       id: page.id,
+       title: null,
+       slug: null,
+       createdTs: null,
+       lastEditedTs: null,
+       contents: []
+      })
+      return;
+    }
+
+    let title: string | null = null
+
+    if(page.properties['Name'].type === 'title'){
+      title = page.properties['Name'].title[0]?.plain_text ?? null ;
+    }
+
+    let slug: string | null  = null;
+ 
+    if(page.properties['Slug'].type === 'rich_text') {
+      slug = page.properties['Slug'].rich_text[0]?.plain_text ?? null ;
+    }
+
+    posts.push({
+      id: page.id,
+      title,
+      slug,
+      createdTs: page.created_time,
+      lastEditedTs: page.last_edited_time,
+      contents: []
+    });
+  
+  });
+
+  return posts;
+}
+
+export const getPostContents = async (post: Post) => {
+    const blockResponses = await notion.blocks.children.list({
+      block_id: post.id
+    });
+    const contents: Content[] = [];
+
+    blockResponses.results.forEach((block) => {
+      if(!('type' in block)){
+        return ;
+      }
+
+      switch(block.type){
+        case 'paragraph':
+          contents.push({
+            type: 'paragraph',
+            text: block.paragraph.rich_text[0]?.plain_text ?? null
+          });
+          break;
+        case 'heading_2':
+           contents.push({
+             type: 'heading_2',
+             text: block.heading_2.rich_text[0]?.plain_text ?? null
+           });
+           break;
+        case 'heading_3':
+           contents.push({
+             type: 'heading_3',
+             text: block.heading_3.rich_text[0]?.plain_text ?? null
+           });
+           break;
+        case 'quote':
+           contents.push({
+             type: 'heading_3',
+             text: block.quote.rich_text[0]?.plain_text ?? null
+           });
+           break;
+        case 'code':
+           contents.push({
+             type: 'code',
+             text: block.code.rich_text[0]?.plain_text ?? null,
+             language: block.code.language
+           });
+      }
+  });
+  return contents;
+}
+
 type StaticProps = {
   posts: []
 }
-
-const notion = new Client({
-    auth: process.env.NOTION_TOKEN
-});
 
 export const getStaticProps: GetStaticProps<StaticProps> = async () => {
     const database = await notion.databases.query({
@@ -78,8 +206,6 @@ export const getStaticProps: GetStaticProps<StaticProps> = async () => {
          lastEditedTs: null,
          contents: []
         })
-
-        
         return;
       }
 
---
+++ nextjs-sample/src/pages/post/[slug].tsx
@@ -0,0 +1,83 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Post, getPostContents,getPosts } from '..'
+import { NextPage } from 'next';
+
+type StaticProps = {
+    post?: Post;
+}
+
+
+type StaticPathsPrams = {
+    slug: string;
+}
+
+export const getStaticProps: GetStaticProps<
+    StaticProps,
+    StaticPathsPrams
+> = async({
+    params,
+    preview
+}) => {
+    const notFoundProps = {
+        props: {},
+        redirect: {
+            destination: '/404'
+        }
+    };
+
+    if(!params) {
+        return notFoundProps;
+    }
+
+    const { slug } = params;
+    const posts = await getPosts(slug);
+    const post = posts.shift();
+    if(!post) {
+        return notFoundProps;
+    }
+    const contents = await getPostContents(post);
+    post.contents = contents;
+    
+    return {
+        props: {
+            post
+        }
+    }
+}
+
+
+export const getStaticPaths: GetStaticPaths<
+    StaticPathsPrams
+> = async () => {
+    const posts = await getPosts();
+    const paths: {
+        params: { slug: string };
+    }[] = [];
+
+    posts.forEach((post, i) => {
+        const slug = post.slug;
+        if(slug) {
+            paths.push({
+                params: {
+                    slug
+                }
+            })
+        }
+    });
+
+    console.dir(paths)
+
+ 
+    return {
+        paths,
+        fallback: 'blocking'
+    }
+}
+
+const PostPage: NextPage<StaticProps> = ({ post }) => {
+    if(!post) return null;
+
+    return <div>{JSON.stringify(post)}</div>
+}
+
+export default PostPage;
