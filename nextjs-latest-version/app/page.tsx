83c2330e4a87da9ec462cd08e2861d4e5e7e42b7
tochisuke221 <tochisuke221@gmail.com>
2023-09-16 16:53:37 +0900

    画像を表示

--- nextjs-latest-version/app/page.tsx
+++ nextjs-latest-version/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "/next/image";
+import Image from "@/node_modules/next/image";
 
 type Photo = {
   id: string;
@@ -7,6 +7,7 @@ type Photo = {
   height: number;
   color: string;
   description: string;
+  alt_description: string;
   urls: {
     raw: string;
     full: string;
@@ -30,6 +31,7 @@ const getRandomPhotos = async (): Promise<Photo[]> => {
   );
 
   params.append('count', '32');
+
   const response = await fetch(
     `https://api.unsplash.com/photos/random?${params.toString()}`,
     { method: 'GET', cache: 'no-cache' }
@@ -42,16 +44,20 @@ const Home = async () => {
   const randomPhotos = await getRandomPhotos();
 
   return (
-    <div>
-      {randomPhotos.map(photo => (
-        <Image
-        key={photo.id}
-        src={photo.urls.small}
-        width={400}
-        height={photo.height * (400 / photo.width)}
-        alt={photo.description}
-      />
-      ))}
+    <div className="grid grid-cols-3 gap-4 w-[1200px] mx-auto">
+      {
+        [0, 1, 2].map((index) => (
+          <div key={randomPhotos[index].id}>
+            <Image
+              key={randomPhotos[index].id}
+              src={randomPhotos[index].urls.small}
+              width={400}
+              height={randomPhotos[index].height * (400 / randomPhotos[index].width)}
+              alt={randomPhotos[index].alt_description}
+            />
+          </div>
+        ))
+      }
     </div>
   )
 }
