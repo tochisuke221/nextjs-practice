e742d51baa14326605e4fd5863680764dca5ab0b
tochisuke221 <tochisuke221@gmail.com>
2023-09-16 17:16:04 +0900

    Interが読み込めない、、、

--- nextjs-latest-version/app/layout.tsx
+++ nextjs-latest-version/app/layout.tsx
@@ -1,44 +1,49 @@
-import { Suspense } from "react";
-import './global.css';
+import { Inter } from '@next/font'
+import { Suspense } from 'react';
+import './globals.css';
+
+const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap'
+});
 
 export default function RootLayout({
-  children
+    children
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="ja">
-      <body>
-        <header
-          className={`
-            h-16
-            bg-transparent
-            background-blur-md
-            flex
-            fixed
-            w-full
-            px-6
-          `}
-        >
-          <div
-            className={`
-              h-auto
-              my-auto
-              font-bold
-              text-5xl
-              tracking-tighter
-            `}
-          >
-            Search Image
-          </div>
-        </header>
-        <main
-        className="pt-20 pb-8 bg-gray-950 min-h-screen">
-          <Suspense fallback={'loading...'}>
-            {children}
-          </Suspense>
-        </main>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="ja" className={inter.className}>
+            <body>
+                <header
+                    className={`
+                        h-16 
+                        bg-transparent 
+                        backdrop-blur-md 
+                        flex 
+                        fixed 
+                        w-full 
+                        px-6
+                    `}
+                >
+                    <div
+                        className={`
+                            h-auto 
+                            my-auto 
+                            font-bold 
+                            text-5xl 
+                            tracking-tighter
+                        `}
+                    >
+                        Search Image
+                    </div>
+                </header>
+                <main className="pt-20 pb-8 bg-gray-950 min-h-screen">
+                    <Suspense fallback={"..loading"}>
+                        {children}
+                    </Suspense>
+                </main>
+            </body>
+        </html>
+    );
 }
